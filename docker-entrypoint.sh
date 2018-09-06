c4b746d7cea1fd658b35c843398c491d933f8c9c
Daniel Ryan <daniel.ryan@processmaker.com>
2018-09-06 15:55:07 +0000

    decompress workspace and plugins. Update php.ini memory limit to 512MB. Restore enterprise workspace with plugins

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,9 +16,22 @@ sed -i '/;opcache.use_cwd=1/c\opcache.use_cwd=1' /etc/php.d/10-opcache.ini
 sed -i '/;opcache.validate_timestamps=1/c\opcache.validate_timestamps=1' /etc/php.d/10-opcache.ini
 sed -i '/;opcache.fast_shutdown=0/c\opcache.fast_shutdown=1' /etc/php.d/10-opcache.ini
 
-# Decompress ProcessMaker
+# Decompress ProcessMaker and Plugins
 cd /tmp && tar -C /opt -xzvf processmaker-3.2.3.tar.gz
-chown -R nginx. /opt/processmaker
+cd /tmp && tar -C /opt/processmaker/workflow/engine -xzvf plugins.tar.gz
+cd /tmp && tar -C /tmp -xzvf bundle.tar.gz
+
+# Update memory to 512MB
+sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php.ini
+
+# Create shared sites dir
+mkdir /opt/processmaker/shared/sites
+
+# Workspace Restore for Enterprise Bundle
+cd /opt/processmaker && ./processmaker workspace-restore -o /tmp/workflow.tar
+
+# Give Nginx ownership of new files
+chown -R nginx:nginx /opt/processmaker
 
 # Start services
 cp /etc/hosts ~/hosts.new
@@ -28,3 +41,5 @@ chkconfig sendmail on && service sendmail start
 chkconfig nginx on && chkconfig php-fpm on
 touch /etc/sysconfig/network
 service php-fpm start && nginx -g 'daemon off;'
+
+
