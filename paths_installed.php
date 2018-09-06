9a06b6389c145392ec112dc73d3cac83b89284cd
Daniel Ryan <daniel.ryan@processmaker.com>
2018-09-06 17:56:15 +0000

    add paths_installed.php configuration file

---
+++ paths_installed.php
@@ -0,0 +1,5 @@
+<?php
+  define('PATH_DATA',         '/opt/processmaker/shared/');
+  define('PATH_C',            '/opt/processmaker/shared/compiled/');
+  define('HASH_INSTALLATION', 'oLKspp1tmpiXZMeYzGicmpudmZxom8SalWaanJOUmpxknZqYZKbQ0tppn2qXbJfIm5XJbJlul8iXlZxwZJrCxpVpcm5imMLGmYCzYqp6hsao2NpnpZo');
+  define('SYSTEM_HASH',       '99514acf0f5f86e5ec736acb0991dac3');
