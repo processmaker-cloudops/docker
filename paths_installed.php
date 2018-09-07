f315fa7b3925aad8a8d9469c71f7dbbdbb4ca542
ProcessMaker CloudOps <36670265+processmaker-cloudops@users.noreply.github.com>
2018-09-07 07:05:09 -0400

    Update paths_installed

--- paths_installed.php
+++ paths_installed.php
@@ -1,6 +1,5 @@
 <?php
   define('PATH_DATA',         '/opt/processmaker/shared/');
   define('PATH_C',            '/opt/processmaker/shared/compiled/');
-
-define ( 'HASH_INSTALLATION','n96n0qSZa8NvlpnDcJvGxnJtl8idmWvIm52enWdtmplimmjCaqam0avHlsJxbZvHcJaVxJ2enMpwbJmblJpsxmmdmploaWvDaabX17Ggspiuenqk3Z2ynmufuZyjmmjCaplrw2+WmcNwm8bGcm2XyJ2Za8ibnZ6dZ22amWJm' );
-define ( 'SYSTEM_HASH', 'e4a847b7bba99de943cde8e78f8054a2' );
+  define('HASH_INSTALLATION', 'n96n0qSZa8NvlpnDcJvGxnJtl8idmWvIm52enWdtmplimmjCaqam0avHlsJxbZvHcJaVxJ2enMpwbJmblJpsxmmdmploaWvDaabX17Ggspiuenqk3Z2ynmufuZyj');
+  define('SYSTEM_HASH',       'e4a847b7bba99de943cde8e78f8054a2');
