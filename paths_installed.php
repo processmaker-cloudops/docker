c7d3db61b817b879afebf16710fc29512369982d
Daniel <daniel.ryan@310networks.com>
2019-11-05 14:57:36 -0800

    remove paths_installed and queue
    Since this is supposed to be a fresh install of processmaker 3.4.0, a workspace hasn't been made yet, this means that the queue worker can't be started. It will have to be started after the workspace has been made.

--- paths_installed.php
+++
@@ -1,5 +0,0 @@
-<?php
-  define('PATH_DATA',         '/opt/processmaker/shared/');
-  define('PATH_C',            '/opt/processmaker/shared/compiled/');
-  define('HASH_INSTALLATION', '1aalpZrNz5akX8nPq2Wm0plsxNihx2fa2GLLx9jXY2eTqZekZ8XOkqyg1MWoqGfHpqOYlmSfncaWZ5fMyZRrb8luaZKcl8dql5abypeanJptm7Onhrao19mWz9TK1W1plnCXkmqWkpeWYpudlWxvxZppx5yYy27Ly5rJnJvIem+1kWtjocXFiWlpytRbaA');
-  define('SYSTEM_HASH',       '7319da121fd159d76ac3f9ee5ffec66e');
