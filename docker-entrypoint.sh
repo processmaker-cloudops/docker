e1841894455e2b0250eafad0a1126365dcca7a18
Daniel <daniel.ryan@310networks.com>
2019-11-05 15:37:48 -0800

    Update docker-entrypoint.sh
    Change workspace restoration to manual rather than processmaker workspace-restore script.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,12 +21,17 @@ cd /tmp && tar -C /opt -xzvf processmaker-3.4.0.tar.gz
 cd /tmp && tar -C /tmp -xzvf bundle.tar.gz
 cd /tmp && cp paths_installed.php /opt/processmaker/workflow/engine/config/paths_installed.php
 chown -R nginx. /opt/processmaker
+mkdir /opt/processmaker/shared/sites
 
 # Set NGINX server_name
 sed -i 's,server_name ~^.*$;,server_name '"${URL}"';,g' /etc/nginx/conf.d/processmaker.conf
 
 # Workspace restore for Enterprise Bundle
-cd /opt/processmaker && ./processmaker workspace-restore -o /tmp/workflow.tar $WORKSPACE && sleep 5
+cd /tmp && tar -xvf workspace.tar
+mv /tmp/test340.files /opt/processmaker/shared/sites/$WORKSPACE
+mysql -u RDSPortainer -pD9PZ82hadX78dp*3 -h portainer.ckz0mnb6cuna.us-east-1.rds.amazonaws.com $WORKSPACE < /tmp/wf_test340.sql
+
+#cd /opt/processmaker && ./processmaker workspace-restore -o /tmp/workflow.tar $WORKSPACE && sleep 5
 cd /opt/processmaker && ./processmaker flush-cache
 cd /opt/processmaker/workflow/engine/bin
 php -f cron.php calculated && php -f cron.php calculatedapp && php -f cron.php report_by_user +init-date"2018-01-01" && php -f cron.php report_by_process +init-date"2018-01-01"
