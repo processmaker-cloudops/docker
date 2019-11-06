5b5f1e21d03be1e1078a5f6b94daf46db87f8e1e
Daniel <daniel.ryan@310networks.com>
2019-11-05 16:53:31 -0800

    Update docker-entrypoint.sh
    move sed to after tar

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,7 +22,7 @@ cd /tmp && tar -C /tmp -xzvf bundle.tar.gz
 cd /tmp && cp paths_installed.php /opt/processmaker/workflow/engine/config/paths_installed.php
 chown -R nginx. /opt/processmaker
 mkdir /opt/processmaker/shared/sites
-sed -i 's/##WORKSPACE##/'"$WORKSPACE"'/g' /opt/processmaker/shared/sites/$WORKSPACE/db.php
+
 
 # Set NGINX server_name
 sed -i 's,server_name ~^.*$;,server_name '"${URL}"';,g' /etc/nginx/conf.d/processmaker.conf
@@ -30,6 +30,7 @@ sed -i 's,server_name ~^.*$;,server_name '"${URL}"';,g' /etc/nginx/conf.d/proces
 # Workspace restore for Enterprise Bundle
 cd /tmp && tar -xvf workflow.tar
 mv /tmp/test340.files /opt/processmaker/shared/sites/$WORKSPACE
+sed -i 's/##WORKSPACE##/'"$WORKSPACE"'/g' /opt/processmaker/shared/sites/$WORKSPACE/db.php
 mysql -u RDSPortainer -pD9PZ82hadX78dp*3 -h portainer.ckz0mnb6cuna.us-east-1.rds.amazonaws.com -e "CREATE DATABASE IF NOT EXISTS $WORKSPACE"
 mysql -u RDSPortainer -pD9PZ82hadX78dp*3 -h portainer.ckz0mnb6cuna.us-east-1.rds.amazonaws.com $WORKSPACE < /tmp/wf_test340.sql
 
