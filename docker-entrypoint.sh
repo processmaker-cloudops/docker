f57681a4a72ffcd223c27961b21fee3b51f84819
Daniel <daniel.ryan@310networks.com>
2019-11-07 13:12:43 -0800

    Update docker-entrypoint.sh
    switch to new bundle

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -29,10 +29,10 @@ sed -i 's,server_name ~^.*$;,server_name '"${URL}"';,g' /etc/nginx/conf.d/proces
 
 # Workspace restore for Enterprise Bundle
 cd /tmp && tar -xvf workflow.tar
-mv /tmp/test340.files /opt/processmaker/shared/sites/$WORKSPACE
+mv /tmp/bundling340.files /opt/processmaker/shared/sites/$WORKSPACE
 sed -i 's/##WORKSPACE##/'"$WORKSPACE"'/g' /opt/processmaker/shared/sites/$WORKSPACE/db.php
 mysql -u RDSPortainer -pD9PZ82hadX78dp*3 -h portainer.ckz0mnb6cuna.us-east-1.rds.amazonaws.com -e "CREATE DATABASE IF NOT EXISTS wf_$WORKSPACE"
-mysql -u RDSPortainer -pD9PZ82hadX78dp*3 -h portainer.ckz0mnb6cuna.us-east-1.rds.amazonaws.com wf_$WORKSPACE < /tmp/wf_test340.sql
+mysql -u RDSPortainer -pD9PZ82hadX78dp*3 -h portainer.ckz0mnb6cuna.us-east-1.rds.amazonaws.com wf_$WORKSPACE < /tmp/wf_bundling340.sql
 
 #cd /opt/processmaker && ./processmaker workspace-restore -o /tmp/workflow.tar $WORKSPACE && sleep 5
 cd /opt/processmaker && ./processmaker flush-cache
